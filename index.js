0fe53d85b5f9f8ed22b5b59f2e00fc8021bd69b5
Zach Nolan <zach@uplinkconsulting.com>
2018-05-31 20:38:10 -0700

    npm init, initial apollo-server setup

---
+++ index.js
@@ -0,0 +1,35 @@
+const { ApolloServer, gql } = require('apollo-server')
+
+const projects = [
+  {
+    name: 'Stardust',
+    teaser: 'Share reactions and discover amazing new movies and TV shows. Available for iOS and Android.'
+  },
+  {
+    name: 'Astra',
+    teaser: 'Astra is mission control for your finances.'
+  }
+]
+
+const typeDefs = gql`
+  type Project {
+    name: String,
+    teaser: String
+  }
+
+  type Query {
+    projects: [Project]
+  }
+`
+
+const resolvers = {
+  Query: {
+    projects: () => projects
+  }
+}
+
+const server = new ApolloServer({typeDefs, resolvers})
+
+server.listen().then(({url}) => {
+  console.log(`🚀 Server ready at ${url}`);
+})
